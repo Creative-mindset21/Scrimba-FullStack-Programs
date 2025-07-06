7630379218187865c1a86eabfb0db6103a72bd19
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-06 04:14:54 +0100

    Changed color based on who is winning

--- Basketball Scoreboad Project/scripts/main.js
+++ Basketball Scoreboad Project/scripts/main.js
@@ -8,8 +8,24 @@ let guestCount = 0;
 function homeIncreaseCount(amount) {
   homeCount += amount;
   homeBox.textContent = homeCount;
+  updateColor();
 }
 function guestIncreaseCount(amount) {
   guestCount += amount;
   guestBox.textContent = guestCount;
+  updateColor();
+}
+
+function updateColor() {
+  if (parseInt(homeBox.textContent) > parseInt(guestBox.textContent)) {
+    homeBox.classList.add("green");
+    guestBox.classList.remove("green");
+  } else if (parseInt(homeBox.textContent) < guestBox.textContent) {
+    guestBox.classList.add("green");
+    homeBox.classList.remove("green");
+  } else {
+    // If scores are equal, remove both classes
+    homeBox.classList.remove("green");
+    guestBox.classList.remove("green");
+  }
 }
