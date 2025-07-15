bbfe46587b08eeee8ac2bebbc8b0916f188cc73f
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-15 04:29:24 +0100

    dynamically added each cards

--- Resturant Ordering Project/script.js
+++ Resturant Ordering Project/script.js
@@ -1,1 +1,26 @@
+import { menuArray } from "./data.js";
+
 const cardsContainer = document.getElementById("cards");
+let htmlString = "";
+
+menuArray.forEach((menu) => {
+  let { name, ingredients, id, price, emoji } = menu;
+  htmlString += `
+        <div class="card">
+            <div class="card-left">
+              <p class="image">${emoji}</p>
+              <div class="text">
+                <h2>${name}</h2>
+                <small>pepperoni,mushroom,mozzarella</small>
+                <p>$${price}</p>
+              </div>
+            </div>
+
+            <button class="card-right">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+        </div>
+    `;
+});
+
+cardsContainer.innerHTML = htmlString;
