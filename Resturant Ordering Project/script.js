4d95418d787a3b1848e05523ab3b26673998abd3
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-15 04:41:38 +0100

    identified the card when clicked

--- Resturant Ordering Project/script.js
+++ Resturant Ordering Project/script.js
@@ -11,16 +11,20 @@ menuArray.forEach((menu) => {
               <p class="image">${emoji}</p>
               <div class="text">
                 <h2>${name}</h2>
-                <small>pepperoni,mushroom,mozzarella</small>
+                <small>${ingredients.join(", ")}</small>
                 <p>$${price}</p>
               </div>
             </div>
 
-            <button class="card-right">
+            <button class="card-right" data-id=${id}>
               <i class="fa-solid fa-plus"></i>
             </button>
         </div>
     `;
+});
+
+document.addEventListener("click", (e) => {
+  const dataId = e.target.closest(".card-right").getAttribute("data-id");
 });
 
 cardsContainer.innerHTML = htmlString;
