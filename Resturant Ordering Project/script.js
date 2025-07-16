7f1ba2ef880dbd70a7e1c7f7d346573d0e4d0912
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-16 05:37:50 +0100

    created the html layout

--- Resturant Ordering Project/script.js
+++ Resturant Ordering Project/script.js
@@ -1,6 +1,9 @@
 import { menuArray } from "./data.js";
 
 const cardsContainer = document.getElementById("cards");
+const wrapper = document.getElementById("order-details");
+const orderEl = document.querySelector(".order");
+
 let htmlString = "";
 
 menuArray.forEach((menu) => {
@@ -23,8 +26,45 @@ menuArray.forEach((menu) => {
     `;
 });
 
-document.addEventListener("click", (e) => {
-  const dataId = e.target.closest(".card-right").getAttribute("data-id");
-});
-
 cardsContainer.innerHTML = htmlString;
+let selectedItems = [];
+
+document.addEventListener("click", (e) => {
+  if (e.target.closest(".card-right")) {
+    const dataId = e.target.closest(".card-right").getAttribute("data-id");
+
+    let selectedItem = menuArray[dataId];
+    let orderString = "";
+
+    if (selectedItem) {
+      selectedItems.push(selectedItem);
+
+      let totalPrice = 0;
+
+      selectedItems.forEach((item) => {
+        orderString += `
+        <div class="detail">
+            <div class="detail-left">
+                <h2>${item.name}</h2>
+                <small role="button">remove</small>
+            </div>
+
+            <p>$${item.price}</p>
+        </div>
+    `;
+      });
+
+      orderString += `
+         <hr />
+
+         <div class="total">
+             <h2>Total price:</h2>
+             <p>$14</p>
+         </div>
+        `;
+    }
+
+    orderEl.style.display = "flex";
+    wrapper.innerHTML += orderString;
+  }
+});
