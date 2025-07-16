600fe3f0a0eb702ac718eb5728df89f765ed6201
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-16 06:31:14 +0100

    added the hex values to the application

--- Color Scheme Generator/script.js
+++ Color Scheme Generator/script.js
@@ -2,25 +2,30 @@ const colorInput = document.getElementById("select-color");
 const colorScheme = document.getElementById("color-scheme");
 const URL = "https://www.thecolorapi.com/scheme";
 const boxes = document.querySelectorAll(".box");
+const textValues = document.querySelectorAll(".value");
+
+document
+  .getElementById("color-btn")
+  .addEventListener("click", generateColorScheme);
 
 function generateColorScheme() {
-  let value = colorInput.value.split("#")[1];
-  let scheme = colorScheme.value;
-  let count = 5;
-  let params = `hex=${value}&mode=${scheme}&count=${count}`;
+  let colorObj = {
+    value: colorInput.value.split("#")[1],
+    scheme: colorScheme.value,
+    count: 5,
+  };
+
+  let params = `hex=${colorObj.value}&mode=${colorObj.scheme}&count=${colorObj.count}`;
 
   fetch(`${URL}?${params}`).then((response) =>
     response.json().then((data) => {
       const colors = data.colors;
       colors.forEach((color, i) => {
         boxes[i].style.background = color.hex.value;
+        textValues[i].textContent = color.hex.value;
       });
     })
   );
 }
 
 generateColorScheme();
-
-document
-  .getElementById("color-btn")
-  .addEventListener("click", generateColorScheme);
