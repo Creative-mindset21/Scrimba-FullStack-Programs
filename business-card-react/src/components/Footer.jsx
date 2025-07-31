119b1e756f98f6d3609fcdabf5c987911ee9f9db
creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-31 12:47:59 +0100

    styled the main section

---
+++ business-card-react/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <FaTwitterSquare />
+      <FaFacebookSquare />
+      <FaInstagramSquare />
+      <FaGithubSquare />
+    </footer>
+  );
+};
+
+export default Footer;
