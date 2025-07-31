a6a20941dfd33b4204a0dcbbd0f254a07727d71c
creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-31 12:12:13 +0100

    first commit

--- Movie Watchlist Project/script.js
+++ Movie Watchlist Project/script.js
@@ -1,11 +1,17 @@
 const trendingCards = document.getElementById("trending-cards");
+const popularCards = document.getElementById("popular-cards");
 
 const TMDB_API = "9f495d6971e6aea88ce74db1d07ab951";
-const URL = "https://api.themoviedb.org/3/trending/movie/week?language=en-US";
+
+const TRENDING_URL =
+  "https://api.themoviedb.org/3/trending/movie/week?language=en-US";
+const POPULAR_URL =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
 const IMAGE_URL = "https://image.tmdb.org/t/p/w185/";
 
-/* RENDER POPULAR MOVIES */
-function renderPopularMovies(arr) {
+/* RENDER TRENDING MOVIES */
+function renderTrendingMovies(arr) {
   let html = "";
   const noMovie = "./assets/no-movie.png";
 
@@ -35,23 +41,69 @@ function renderPopularMovies(arr) {
           </div>
     `;
     trendingCards.innerHTML = html;
-    console.log(movie);
   });
 }
 
-/* GET POPULAR MOVIES */
-const getPopularMovies = async () => {
-  const response = await fetch(URL, {
+/* RENDER POPULAR MOVIES */
+function renderPopularMovies(arr) {
+  let html = "";
+  const noMovie = "./assets/no-movie.png";
+
+  arr.forEach((movie, index) => {
+    const moviePath = movie.backdrop_path
+      ? `${IMAGE_URL}${movie.backdrop_path}`
+      : noMovie;
+    html += `
+        <div class="popular-card card" key=${index}>
+            <img src="${moviePath}" alt="Movie Poster of ${movie.title}" />
+
+            <div class="texts popular">
+              <h3>${movie.title}</h3>
+
+              <div class="ratings">
+                <img src="./assets/star.svg" alt="Star Icon" class="star" />
+                <span class="rate">${movie.vote_average.toFixed(1)} •</span>
+                <span class="lang">${movie.original_language} •</span>
+                <span class="year">${movie.release_date}</span>
+              </div>
+            </div>
+          </div>
+    `;
+    popularCards.innerHTML = html;
+  });
+}
+
+/* GET TRENDING MOVIES */
+const getTrendingMovies = async () => {
+  const options = {
     method: "GET",
     headers: {
       accept: "application/json",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjQ5NWQ2OTcxZTZhZWE4OGNlNzRkYjFkMDdhYjk1MSIsIm5iZiI6MTc1MjgwMTY2Ni4xNjUsInN1YiI6IjY4NzlhMTgyMjIzMTAyYmMzZTU1YzAzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kWh0N2NW4bT4xkSDOOqjb-ts4cz5CRqcPTO1WTDk2Ac",
     },
-  });
+  };
 
+  const response = await fetch(TRENDING_URL, options);
   const data = await response.json();
-  renderPopularMovies(data.results.splice(0, 6));
+  renderTrendingMovies(data.results.splice(0, 6));
+};
+
+/* GET TRENDING MOVIES */
+const getPopularMovies = async () => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjQ5NWQ2OTcxZTZhZWE4OGNlNzRkYjFkMDdhYjk1MSIsIm5iZiI6MTc1MjgwMTY2Ni4xNjUsInN1YiI6IjY4NzlhMTgyMjIzMTAyYmMzZTU1YzAzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kWh0N2NW4bT4xkSDOOqjb-ts4cz5CRqcPTO1WTDk2Ac",
+    },
+  };
+
+  const response = await fetch(POPULAR_URL, options);
+  const data = await response.json();
+  renderPopularMovies(data.results);
 };
 
 getPopularMovies();
+getTrendingMovies();
