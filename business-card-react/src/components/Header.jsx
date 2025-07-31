ba97f240a32a8838fc8dd778d1f64774d28cf7b0
creative-mindset21 <tobiawotoye03@gmail.com>
2025-07-31 12:37:05 +0100

    styled the header section

--- business-card-react/src/components/Header.jsx
+++ business-card-react/src/components/Header.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import img from "../assets/img.png";
+import { IoIosMailUnread } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <img src={img} alt="Profile Image" />
+
+      <div className="texts">
+        <h1>Laura Smith</h1>
+        <small>Frontend Developer</small>
+        <p>laurasmith.website</p>
+
+        <div className="btns">
+          <div className="btn">
+            <IoIosMailUnread />
+            <span>Email</span>
+          </div>
+
+          <div className="linkedin btn">
+            <FaLinkedin />
+            <span>LinkedIn</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
